4fd9a0cf537600d60a67ae2ac89869af01ab0895
jcrn <clemons.jason@gmail.com>
2019-12-24 08:56:48 -0800

    added advanced physics engine, gravity and jump capabilities

--- platformer.py
+++ platformer.py
@@ -99,11 +99,7 @@ class MyGame(arcade.Window):
 
     def  on_key_release(self, key, modifiers):
         """ Called whenever a key is released """
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.A:
+        if key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = 0
